032cf47af954d5363a85cd02d50f8b1d95e188cf
sergey-frey <sergey.frey.work@gmail.com>
2024-11-05 11:58:05 +0300

    fix: types imports

--- src/server/db/supabase.ts
+++ src/server/db/supabase.ts
@@ -1,5 +1,5 @@
+import { Database } from "@/src/shared/types/db.types";
 import { createClient } from "@supabase/supabase-js";
-import { Database } from "../types/db.types";
 
 const supabaseUrl = "https://byscqyjpqifbuwwoazbk.supabase.co";
 const supabaseKey = process.env.SUPABASE_API_KEY;
