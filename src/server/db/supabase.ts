a02a97d421bc3db54e55bf21659df10652c4db0c
sergey-frey <sergey.frey.work@gmail.com>
2025-04-05 09:28:37 +0300

    fix: extract supabaseUrl to env

--- src/server/db/supabase.ts
+++ src/server/db/supabase.ts
@@ -1,8 +1,12 @@
 import { Database } from "@/src/shared/types/db.types";
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://byscqyjpqifbuwwoazbk.supabase.co";
+const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_API_KEY;
+
+if (!supabaseUrl) {
+  throw new Error("SUPABASE_URL is not defined");
+}
 
 if (!supabaseKey) {
   throw new Error("SUPABASE_API_KEY is not defined");
